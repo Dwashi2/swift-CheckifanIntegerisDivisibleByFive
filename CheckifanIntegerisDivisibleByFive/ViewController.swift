69dc7462f3378a4c7c48bc48ceee7f3d3e252efc
Daniel Washington <danielvertigo@hotmail.com>
2021-06-14 16:11:33 -0300

    Files

--- CheckifanIntegerisDivisibleByFive/ViewController.swift
+++ CheckifanIntegerisDivisibleByFive/ViewController.swift
@@ -5,6 +5,23 @@
 //  Created by Daniel Washington Ignacio on 14/06/21.
 //
 
+/*
+ Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+
+ Examples
+
+ divisibleByFive(5) ➞ true
+
+ divisibleByFive(-55) ➞ true
+
+ divisibleByFive(37) ➞ false
+ Notes
+
+ Don't forget to return the result.
+ If you get stuck on a challenge, find help in the Resources tab.
+ If you're really stuck, unlock solutions in the Solutions tab.
+*/
+ 
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +29,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.divisibleByFive(5))
+        print(self.divisibleByFive(-55))
+        print(self.divisibleByFive(37))
+    }
+    
+    func divisibleByFive(_ num: Int) -> Bool {
+        return (num % 5 == 0)
     }
 
 
