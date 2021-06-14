b87e78c5a89719871ea64d2524d2734030a1f854
Daniel Washington <danielvertigo@hotmail.com>
2021-06-14 16:08:07 -0300

    Initial Commit

---
+++ CheckifanIntegerisDivisibleByFive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckifanIntegerisDivisibleByFive
+//
+//  Created by Daniel Washington Ignacio on 14/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
